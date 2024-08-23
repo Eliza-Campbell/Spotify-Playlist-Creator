c54bc18e7cfe974db775a522bfcef098b8fff609
gh0stprncess <20campra@gmail.com>
2024-08-23 14:14:33 +1000

    Add playlist title input

--- src/components/SaveButton.js
+++ src/components/SaveButton.js
@@ -1,8 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 
 function SaveButton({ handleClick, auth }) {
+	const [title, setTitle] = useState("");
+
 	return (
 		<div className="savebuttoncontainer">
+			<input
+				className="titleeditor"
+				type="text"
+				placeholder="Enter playlist title"
+				value={title}
+				onChange={(e) => setTitle(e.target.value)}
+			></input>
 			<button onClick={handleClick} className="savebutton">
 				{[!auth ? "Authorise Spotify" : "Export Playlist"]}
 			</button>
